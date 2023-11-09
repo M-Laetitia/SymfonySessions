3eabbcf439b5418ada6e72b68b56ab3ba9d57241
M-Laetitia <molin.laetitia@gmail.com>
2023-11-09 15:51:02 +0100

    Feat: display/edit/add students

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Student;
+use App\Form\StudentType;
+use App\Repository\StudentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StudentController extends AbstractController
 {
     #[Route('/student', name: 'app_student')]
-    public function index(): Response
+    public function index(StudentRepository $studentRepository): Response
     {
+        $students = $studentRepository->findBy([], ["lastName" => "ASC"]);
         return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
+            // 'controller_name' => 'StudentController',
+            'students' => $students
         ]);
+    }
+
+    #[Route('/student/new', name: 'new_student')]
+    #[Route('/student/{id}/edit', name: 'edit_student')]
+    public function new_edit(Student $student = null, Request $request, EntityManagerInterface $entityManager) : Response
+    {
+
+        if(!$student) {
+            $student = new Student();
+        }
+
+        $form = $this->createForm(StudentType::class, $student);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $student = $form->getData();
+            $entityManager->persist($student);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_student');
+        }
+
+        return $this->render('student/new.html.twig', [
+            'formAddStudent' => $form,
+            'edit' => $student->getId()
+        ]);
+
     }
 }
