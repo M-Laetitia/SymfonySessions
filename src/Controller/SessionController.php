26ed1df46d0a90d6488ea717e9bf7783a30a53f9
M-Laetitia <molin.laetitia@gmail.com>
2023-11-10 00:51:19 +0100

    Feat: display list session + detail session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
 use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,13 +13,23 @@ class SessionController extends AbstractController
    
 
 
-    #[Route('/session', name: 'app_sessiont')]
+    #[Route('/session', name: 'app_session')]
     public function index(SessionRepository $sessionRepository): Response
     {
-        $sessions = $sessionRepository->findBy([], ["lastName" => "ASC"]);
+        $sessions = $sessionRepository->findBy([], ["endDate" => "ASC"]);
         return $this->render('session/index.html.twig', [
             // 'controller_name' => 'SessionController',
             'sessions' => $sessions
+        ]);
+    }
+
+
+
+    #[Route('/session/{id}', name: 'show_session')]
+    public function show(Session $session): Response {
+
+        return $this->render('session/show.html.twig', [
+            'session' => $session
         ]);
     }
 
