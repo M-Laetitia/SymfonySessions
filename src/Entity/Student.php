4506223267ac5a675f6f17e19bf0d6029d1c1dc2
M-Laetitia <molin.laetitia@gmail.com>
2023-11-09 16:02:25 +0100

    Feat: add toString method

--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\StudentRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Session;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\StudentRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: StudentRepository::class)]
 class Student
@@ -156,5 +157,10 @@ class Student
         $this->session->removeElement($session);
 
         return $this;
+    }
+
+    public function __toString() {
+        
+        return $this->firstName. " " .$this->lastName;
     }
 }
