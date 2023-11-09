90291833fb543220091d918c3eaa2c1941aa44d7
M-Laetitia <molin.laetitia@gmail.com>
2023-11-09 09:39:54 +0100

    Feat: add relation with module

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -15,6 +17,14 @@ class Category
 
     #[ORM\Column(length: 50)]
     private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: moduleFormation::class, orphanRemoval: true)]
+    private Collection $moduleFormation;
+
+    public function __construct()
+    {
+        $this->moduleFormation = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class Category
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, moduleFormation>
+     */
+    public function getModuleFormation(): Collection
+    {
+        return $this->moduleFormation;
+    }
+
+    public function addModuleFormation(moduleFormation $moduleFormation): static
+    {
+        if (!$this->moduleFormation->contains($moduleFormation)) {
+            $this->moduleFormation->add($moduleFormation);
+            $moduleFormation->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeModuleFormation(moduleFormation $moduleFormation): static
+    {
+        if ($this->moduleFormation->removeElement($moduleFormation)) {
+            // set the owning side to null (unless already changed)
+            if ($moduleFormation->getCategory() === $this) {
+                $moduleFormation->setCategory(null);
+            }
+        }
 
         return $this;
     }
