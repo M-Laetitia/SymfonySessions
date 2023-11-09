af44dce33308b6c21b5903959f90ead9cef342eb
M-Laetitia <molin.laetitia@gmail.com>
2023-11-09 23:59:07 +0100

    Feat: display sessions (not working)

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SessionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
-    #[Route('/session', name: 'app_session')]
-    public function index(): Response
+   
+
+
+    #[Route('/session', name: 'app_sessiont')]
+    public function index(SessionRepository $sessionRepository): Response
     {
+        $sessions = $sessionRepository->findBy([], ["lastName" => "ASC"]);
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            // 'controller_name' => 'SessionController',
+            'sessions' => $sessions
         ]);
     }
+
 }
