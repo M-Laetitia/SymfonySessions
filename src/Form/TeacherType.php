24052f683eaeff77ddcc414d9ae47181575b0a2b
M-Laetitia <molin.Laetitia@gmail.com>
2023-11-10 11:22:43 +0100

    Feat: add a form to edit teacher

---
+++ src/Form/TeacherType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class TeacherType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class)
+            // ->add('roles', TextType::class)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            // ->add('moduleFormations')
+
+            ->add('Validate', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
