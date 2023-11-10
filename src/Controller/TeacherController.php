6f890d8d5c0852a4d98d2fda375460f753e0c355
M-Laetitia <molin.Laetitia@gmail.com>
2023-11-10 11:21:49 +0100

    Feat: edit/delete e teacher (not working atm)

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\TeacherType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,7 +36,6 @@ class TeacherController extends AbstractController
             //     'role_user' => '%"ROLE_USER"%',
             // ])
 
-
             ->orderBy('u.lastName', 'ASC')
             ->getQuery()
             ->getResult();
@@ -42,6 +43,54 @@ class TeacherController extends AbstractController
         return $this->render('teacher/index.html.twig', [
             'controller_name' => 'TeacherController',
             'teachers' => $teachers,
+        ]);
+    }
+
+    #[Route('/teacher/new', name: 'new_teacher')]
+    #[Route('/teacher/{id}/edit', name: 'edit_teacher')]
+    public function new_edit(User $user = null, Request $request, EntityManagerInterface $entityManager) : Response
+    {
+
+        if(!$user) {
+            $user = new User();
+        }
+
+        $form = $this->createForm(TeacherType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_teacher');
+        }
+
+        return $this->render('teacher/new.html.twig', [
+            'formAddTeacher' => $form,
+            'edit' => $user->getId()
+        ]);
+
+    }
+
+    #[Route('/teacher/{id}/delete', name: 'delete_teacher')]
+    public function delete(User $user, EntityManagerInterface $entityManager) : Response {
+
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_teacher');
+    }
+
+    
+    #[Route('/teacher/{id}', name: 'show_teacher')]
+    public function show(User $user): Response {
+
+        return $this->render('teacher/show.html.twig', [
+            'user' => $user
         ]);
     }
 }
