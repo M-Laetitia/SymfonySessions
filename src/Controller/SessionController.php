6ebec2c330dacab6606a4de18e03ef95eca8bce1
M-Laetitia <molin.Laetitia@gmail.com>
2023-11-10 09:35:11 +0100

    Feat: add/edit/delete session

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Form\SessionType;
 use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,6 +26,46 @@ class SessionController extends AbstractController
         ]);
     }
 
+    #[Route('/session/new', name: 'new_session')]
+    #[Route('/session/{id}/edit', name: 'edit_session')]
+    public function new_edit(Session $session = null, Request $request, EntityManagerInterface $entityManager) : Response
+    {
+
+        if(!$session) {
+            $session = new session();
+        }
+
+        $form = $this->createForm(SessionType::class, $session);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $session = $form->getData();
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session');
+        }
+
+        return $this->render('session/new.html.twig', [
+            'formAddSession' => $form,
+            'edit' => $session->getId()
+        ]);
+
+    }
+
+
+    #[Route('/session/{id}/delete', name: 'delete_session')]
+    public function delete(Session $session, EntityManagerInterface $entityManager) : Response {
+
+        // remove: enlève de la collection l'employé ciblé
+        // flush : fait la requête SQl auprès de la BD
+        $entityManager->remove($session);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_session');
+    }
 
 
     #[Route('/session/{id}', name: 'show_session')]
