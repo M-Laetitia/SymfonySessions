96e60a4f4b410133b75678ae73c8bfe406de7882
M-Laetitia <molin.laetitia@gmail.com>
2023-11-12 03:52:33 +0100

    Refactor: add Type to the form + detail

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -2,22 +2,48 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Session;
+use App\Entity\Formation;
+
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
 
 class SessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startDate')
-            ->add('endDate')
-            ->add('nbPlaceTotal')
-            ->add('students')
-            ->add('formation')
-            ->add('user')
+            // ->add('formation' , TextType::class)
+
+            ->add('formation', EntityType::class, [
+                'class' => Formation::class,
+                // 'choice_label' => 'name',
+            ])
+
+            ->add('name', TextType::class)
+            ->add('startDate' , DateType::class)
+            ->add('endDate' , DateType::class)
+            ->add('nbPlaceTotal' , IntegerType::class)
+
+
+         
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                // 'choice_label' => 'title',
+            ])
+
+            
+
+            ->add('Validate', SubmitType::class)
         ;
     }
 
