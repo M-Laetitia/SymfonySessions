750bb4864014fbcc3879592d9027151017f591a2
M-Laetitia <molin.laetitia@gmail.com>
2023-11-13 00:08:17 +0100

    push

--- 01-Annexes/test.php
+++ 01-Annexes/test.php
@@ -27,3 +27,55 @@ $builder
                 ],
             ])
         ;
+
+
+        https://github.com/baku67/ELAN_Symfony_Sessions/blob/master/templates/base.html.twig
+
+
+        <h3>Programmes</h3>
+            <ul class="programmes" data-prototype="{{ form_widget(formAddSession.programmes.vars.prototype)|e('html_attr') }}">
+                {% for programmeForm in formAddSession.programmes %}
+                    <li>
+                        {{ form_row(programmeForm.moduleFormation.name) }}
+                        {{ form_row(programmeForm.numberOfDays) }}
+                        <button type="button" class="btn btn-danger" onclick="removeProgramme(this)">Remove</button>
+                    </li>
+                {% endfor %}
+                <li><button type="button" class="btn btn-success" onclick="addProgramme()">Add Programme</button></li>
+            </ul>
+        
+            {{ form_row(formAddSession.Validate) }}
+        
+        {{ form_end(formAddSession) }}
+        
+        <script>
+            function addProgramme() {
+                var prototype = document.querySelector('.programmes').getAttribute('data-prototype');
+                var index = document.querySelectorAll('.programmes li').length;
+                var newForm = prototype.replace(/__name__/g, index);
+                document.querySelector('.programmes').insertAdjacentHTML('beforeend', newForm);
+            }
+        
+            function removeProgramme(button) {
+                button.parentElement.remove();
+            }
+        </script>
+
+
+#[Route('/session/{id}/delete/{studentId}', name: 'remove_student_from_session')]
+    public function removeStudent($id, $studentId, EntityManagerInterface $entityManager): Response
+    {
+       // Perform a raw SQL DELETE query to remove the student from the session
+    $sql = 'DELETE FROM Student_Session WHERE session_id = :sessionId AND student_id = :studentId';
+    $params = ['sessionId' => $id, 'studentId' => $studentId];
+
+    try {
+        $statement = $entityManager->getConnection()->prepare($sql);
+        $statement->execute($params);
+    } catch (\Exception $e) {
+        // Handle exceptions if needed
+    }
+
+    // Redirect back to the session page
+    return $this->redirectToRoute('show_session', ['id' => $id]);
+    }
