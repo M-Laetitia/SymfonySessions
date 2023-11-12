8f4c49ba68334fdc1285a59d9d669b53f595501e
M-Laetitia <molin.laetitia@gmail.com>
2023-11-12 11:43:11 +0100

    Refactor: fix typo in the setModuleFormation

--- src/Entity/Programme.php
+++ src/Entity/Programme.php
@@ -1,10 +1,10 @@
 <?php
-
+ 
 namespace App\Entity;
-
+ 
 use App\Repository\ProgrammeRepository;
 use Doctrine\ORM\Mapping as ORM;
-
+ 
 #[ORM\Entity(repositoryClass: ProgrammeRepository::class)]
 class Programme
 {
@@ -12,56 +12,56 @@ class Programme
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+ 
     #[ORM\Column]
     private ?int $duration = null;
-
+ 
     #[ORM\ManyToOne(inversedBy: 'programmes')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Session $session = null;
-
+ 
     #[ORM\ManyToOne(inversedBy: 'programmes')]
     #[ORM\JoinColumn(nullable: false)]
     private ?ModuleFormation $moduleFormation = null;
-
+ 
     public function getId(): ?int
     {
         return $this->id;
     }
-
+ 
     public function getDuration(): ?int
     {
         return $this->duration;
     }
-
+ 
     public function setDuration(int $duration): static
     {
         $this->duration = $duration;
-
+ 
         return $this;
     }
-
+ 
     public function getSession(): ?Session
     {
         return $this->session;
     }
-
+ 
     public function setSession(?Session $session): static
     {
         $this->session = $session;
-
+ 
         return $this;
     }
-
+ 
     public function getModuleFormation(): ?ModuleFormation
     {
         return $this->moduleFormation;
     }
-
-    public function setModuleFormation(?ModuleFormation $moduleFormation): static
+ 
+    public function setModuleFormation(?ModuleFormation $moduleFormation): self
     {
-        $this->$moduleFormation = $moduleFormation;
-
+        $this->moduleFormation = $moduleFormation;
+ 
         return $this;
     }
 }
