79ed04ef513709613e402758617a8253e305d07a
M-Laetitia <molin.laetitia@gmail.com>
2023-11-12 11:53:45 +0100

    Feat: add the delete programme method

--- src/Controller/ProgrammeController.php
+++ src/Controller/ProgrammeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Programme;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProgrammeController extends AbstractController
 {
@@ -16,6 +18,22 @@ class ProgrammeController extends AbstractController
         ]);
     }
 
-   
+
+    #[Route('/programme/{id}/delete', name: 'delete_programme')]
+    public function delete(Programme $programme, EntityManagerInterface $entityManager): Response
+    {
+        // Get the associated Session
+        $session = $programme->getSession();
+
+        // Remove the Programme from the Session
+        $session->removeProgramme($programme);
+
+        // Persist and flush changes
+        $entityManager->persist($session);
+        $entityManager->flush();
+
+        // Redirect back to the session or wherever you want
+        return $this->redirectToRoute('show_session', ['id' => $session->getId()]);
+    }
 
 }
