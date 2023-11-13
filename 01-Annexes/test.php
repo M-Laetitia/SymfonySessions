841f92a118a4073178da77f4ba1614e5231f0e42
M-Laetitia <molin.Laetitia@gmail.com>
2023-11-13 16:43:27 +0100

    push

--- 01-Annexes/test.php
+++ 01-Annexes/test.php
@@ -86,3 +86,68 @@ $builder
 // ./dart-sass/sass --help
 // ./dart-sass/sass public/sass/style.scss style.css
 // ./dart-sass/sass public/sass/style.scss style.css --watch
+
+
+
+<?php 
+#[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Veuillez saisir un intitulé')]
+    private ?string $intitule = null;
+
+
+
+
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\NotBlank(message: 'Veuillez sélectionner une date de début')]
+    #[Assert\GreaterThanOrEqual(
+        "today",
+        message: 'La date doit être supérieure ou égale à la date courante'
+    )]
+    #[Assert\When(
+        expression: 'this.getDateEnd() != null',
+        constraints: [
+            new Assert\LessThan(
+                propertyPath: 'dateEnd',
+                message: 'La date de fin doit être ultérieure à la date de début'
+            )
+        ]
+    )]
+    private ?\DateTimeInterface $dateStart = null;
+
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\NotBlank(message: 'Veuillez sélectionner une date de fin')]
+    #[Assert\When(
+        expression: 'this.getDateStart() != null',
+        constraints: [
+            new Assert\GreaterThan(
+                propertyPath: 'dateStart',
+                message: 'La date de début doit être antérieure à la date de fin'
+            )
+        ]
+    )]
+    private ?\DateTimeInterface $dateEnd = null;
+
+
+
+    #[ORM\Column]
+    #[Assert\Type(type:'int', message: 'Veuillez saisir une valeur numérique entière')]
+    #[Assert\GreaterThan(value: 0, message: 'La valeur doit être supérieure à 0')]
+    private ?int $nbPersonnes = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Regex(
+        pattern: "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{6,}$/",
+        message: 'Le code de réservation doit contenir au moins une majuscules, une minuscule, un chiffre, un caratère spécial et 6 caractères minimum'
+    )]
+    private ?string $codeReservation = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\EqualTo(
+        propertyPath: 'codeReservation',
+        message: 'Les 2 codes de réservation doivent correspondre'
+    )]
+    private ?string $confirmCodeReservation = null;
+
+    ?>
