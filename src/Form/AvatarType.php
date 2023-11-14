a67fd837912d0f3b82b749ac9bbd9f3456884ec3
M-Laetitia <molin.laetitia@gmail.com>
2023-11-15 00:17:14 +0100

    Feat: add avatar form

---
+++ src/Form/AvatarType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AvatarType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('avatarFile', FileType::class, [
+                'required' => false,
+                'label' => 'Avatar',
+                // 'allow_delete' => true,
+                // 'download_label' => 'Download',
+                // 'download_uri' => true,
+                // 'image_uri' => true,
+
+                
+            ])
+            
+            ->add('Enregistrer', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
