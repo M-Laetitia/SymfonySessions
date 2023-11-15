1c03b7dbfc73f8f359d80062870418c219487356
M-Laetitia <molin.Laetitia@gmail.com>
2023-11-15 11:33:03 +0100

    Feat: add avatar form

--- src/Form/AvatarType.php
+++ src/Form/AvatarType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -16,12 +16,32 @@ class AvatarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('avatarFile', VichFileType::class, [
+            ->add('avatar',FileType::class, [
+                'required' => false,
+                'label' => 'profil picture',
+                'data_class' => null,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
                 'required' => false,
 
-            ]);
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'extensions' => [
+                            'png',
+                            'jpeg',
+                            'jpg',
+                        ],
+                        'extensionsMessage' => 'Please upload a valid image (autorized files: png, jpeg, jpg - max size : 1024k)',
+                    ])
+                ],
+
+            ])
             
-            // ->add('Enregistrer', SubmitType::class);
+            ->add('Enregistrer', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
