93aa439c8ef3e1bd8d433b68fb829aae2fca7478
M-Laetitia <molin.laetitia@gmail.com>
2023-11-18 23:42:38 +0100

    Style: maintain change on pages

--- public/js/script.js
+++ public/js/script.js
@@ -59,6 +59,30 @@
     
     });
 
+    //^ Keep style on pages ---------------------
+
+    $(document).ready(function() {
+        // Récupérer le chemin de la page actuelle
+        var path = window.location.pathname;
+        path = path.replace(/\/$/, "");
+        path = decodeURIComponent(path);
+        
+      
+        // Parcourir chaque lien du menu
+        $('#nav-links a').each(function() {
+          var href = $(this).attr('href');
+          // Vérifier si le lien correspond à la page actuelle
+          if (path === href) {
+            // Ajouter la classe "active" à l'élément parent (li)
+            $(this).addClass('activemenu');
+            var iconElement = $(this).prev('i');
+            $(iconElement).addClass('activeIcon');
+
+          }
+        });
+      });
+
+
     //^ change color label when focus on input ---------------------
     $(document).ready(function () {
         console.log("test")
@@ -78,4 +102,57 @@
             var input = $(this);
             $(input).toggleClass('inputChange')
         })
+    });
+
+
+    //^ show/ hide sessions ---------------------
+
+    $(document).ready(function () {
+            
+        $('#currentUpBtn').click(function () {
+            console.log("ok")
+                $('#current-session-list').slideToggle();
+                $('#currentUpBtn').toggleClass('rotated');
+            },
+            
+        );
+
+        $('#upcomingUpBtn').click(function () {
+            console.log("ok")
+                $('#upcoming-session-list').slideToggle();
+                $('#upcomingUpBtn').toggleClass('rotated');
+            },
+            
+        );
+
+    });
+
+    // lazy loading past sessions
+
+    $(document).ready(function () {
+        // Lorsque le bouton est cliqué
+        $('#upcomingUpBtn').click(function () {
+            // Vérifier si les sessions à venir n'ont pas déjà été chargées
+            if (!$('#upcoming-session-list').hasClass('sessions-loaded')) {
+                // Charger les sessions à venir
+                $.ajax({
+                    url: '/', // Remplacez par votre URL de chargement des données
+                    method: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
+                        // Manipuler les données JSON (par exemple, les ajouter à la page)
+                    displayUpcomingSessions(data);
+    
+                        // Ajouter une classe pour indiquer que les sessions à venir ont été chargées
+                        $('#upcoming-session-list').addClass('sessions-loaded');
+                    },
+                    error: function (error) {
+                        console.error('Erreur lors du chargement des sessions à venir :', error);
+                    }
+                });
+            }
+    
+            // Afficher ou masquer les sessions à venir en fonction de l'état actuel
+            $('#upcoming-session-list').slideToggle();
+        });
     });
